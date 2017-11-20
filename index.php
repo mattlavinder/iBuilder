fa811a31e2c00aa43e5a5b513674b20171333932
Matt Lavinder <mlavinder@data-management.com>
2017-11-20 10:04:57 -0500

    Updated to support compiling on local environment or system

--- index.php
+++ index.php
@@ -12,18 +12,40 @@ class Builder
     const DIAGNOSTIC_END = "* * * * *   E N D   O F   A D D I T I O N A L   D I A G N O S T I C   M E S S A G E S   * * * * *";
     const SUMMARY = "M e s s a g e   S u m m a r y";
 
+    protected $isPASE = false;
+    protected $variables = [];
+    
+    public function __construct() {
+        if (PHP_OS == 'AIX') {
+            $this->isPASE = true;
+        }
+        $this->variables['TARGET_DIR'] = $_SERVER['TARGET_DIR'];
+        $this->variables['BIN_LIB'] = $_SERVER['BIN_LIB'];
+        $this->variables['SYSTEM_HOST'] = $_SERVER['SYSTEM_HOST'];
+        $this->variables['SYSTEM_USER'] = $_SERVER['SYSTEM_USER'];
+    }
+    
     static function run($argv) {
         $instance = new self();
         $instance->execute($argv);
     }
 
     function command($command) {
+        echo "$command\n";
         exec($command, $output, $status);
         if ($status != 0) {
             $this->display($output);
             exit ($status);
         }
         return $output;
+    }
+    
+    function remoteCommand($command) {
+    
+        $this->variables['COMMAND'] = str_replace('"', '\"', $command);
+        $sshCommand = 'ssh {SYSTEM_USER}@{SYSTEM_HOST} "{COMMAND}"';
+        $sshCommand = $this->parseCommand($sshCommand);
+        return $this->command($sshCommand);
     }
 
     function display($output) {
@@ -51,30 +73,46 @@ class Builder
             }
         }
     }
-
-    function execute($argv) {
-        
-        $object = $argv[1];
-        $source = $argv[2];
-        $command = $argv[3];
-
-        $host = $_SERVER['SYSTEM_HOST'];
-        $user = $_SERVER['SYSTEM_USER'];
-        $directory = $_SERVER['TARGET_DIR'];
-        $library = $_SERVER['BIN_LIB'];
-
-        $command = str_replace('{OBJECT}', $object, $command);
-        $command = str_replace('{SOURCE}', $source, $command);
-        $command = str_replace('{BIN_LIB}', $library, $command);
-        $command = str_replace('{TARGET_DIR}', $directory, $command);
-
-        echo "Uploading $source to $host...\n";
-        $output = $this->command("scp $source $user@$host:$directory");
+    
+    function parseCommand($command) {
+        foreach ($this->variables as $key => $value) {
+            $command = str_replace('{'.$key.'}', $value, $command);
+        }
+        return $command;
+    }
+    
+    function uploadSource() {
+        echo "Uploading " . $this->variables['SOURCE'] . " to ".$this->variables['SYSTEM_HOST'] . "...\n";
+        $scpCommand = $this->parseCommand("scp {SOURCE} {SYSTEM_USER}@{SYSTEM_HOST}:{TARGET_DIR}");
+        $output = $this->command($scpCommand);
         $this->display($output);
         echo "Complete\n";
+    }
 
-        echo "Compiling $source on $host...\n";
-        $output = $this->command('ssh '.$user.'@'. $host . ' "system \"'. $command . '\""');
+    function execute($argv) {
+    
+        $this->variables['OBJECT'] = $argv[1];
+        $this->variables['SOURCE'] = $argv[2];
+        $command = $argv[3];
+        
+        
+        $command = $this->parseCommand($command);
+
+        if (!$this->isPASE) {
+            $this->uploadSource();
+            echo "Compiling ".$this->variables['SOURCE']." on ".$this->variables['SYSTEM_HOST'] ."...\n";
+        }
+        else {
+            echo "Compiling ".$this->variables['SOURCE'] ."...\n";
+        }
+        
+        $command = 'system "'. $command . '"';
+        if ($this->isPASE) {
+            $output = $this->command($command);
+        }
+        else {
+            $output = $this->remoteCommand($command);
+        }
         $this->displayListing($output);
     }
 }
